5d4ffee794a66f79a58c23563e4c341d556382ef
LarisaZu <zueval@icloud.com>
2021-10-22 21:51:53 +0300

    add validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const ctrl = require('../../controllers/contacts')
+const { addContactsValidation, patchContactsValidation } = require('../../validation/validation')
 
 router.get('/', ctrl.listContacts)
 router.get('/:contactId', ctrl.getContactById)
-router.post('/', ctrl.addContact)
+router.post('/', addContactsValidation, ctrl.addContact)
 router.delete('/:contactId', ctrl.removeContact)
-router.put('/:contactId', ctrl.updatePutContact)
-router.patch('/:contactId', ctrl.updatePatchContact)
+router.put('/:contactId', addContactsValidation, ctrl.updatePutContact)
+router.patch('/:contactId', patchContactsValidation, ctrl.updatePatchContact)
 
 module.exports = router
